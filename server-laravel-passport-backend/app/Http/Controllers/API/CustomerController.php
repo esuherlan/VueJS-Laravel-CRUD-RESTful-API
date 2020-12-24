a7defb3811abda49193e0b64e9b9cccec70033ae
esuherlan <elansuherlan.es27@gmail.com>
2020-12-24 22:24:40 +0700

    some code update (frontend and oauth configuration etc)

---
+++ server-laravel-passport-backend/app/Http/Controllers/API/CustomerController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Customer;
+use Illuminate\Support\Facades\Validator;
+
+class CustomerController extends Controller
+{
+    //
+}
---
+++ server-laravel-passport-backend/app/Http/Controllers/API/PassportAuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use App\Models\OauthToken;
+
+class PassportAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:4',
+            'email' => 'required|email',
+            'password' => 'required|alpha_num|min:5'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json(["validation_errors" => $validator->errors()]);
+        }
+  
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+  
+        $token = $user->createToken('token')->accessToken;
+        $userToken = OauthToken::create([
+            'user_id' => $user->id,
+            'access_token' => $token
+        ]);
+
+        if(!is_null($user)) {
+            return response()->json([
+                'status' => [
+                    'code' => 200,
+                    'response' => 'success',
+                    'message' => 'Register new user successfully.'
+                ],
+                'result' => $user
+            ]);
+        } else {
+            return response()->json([
+                'status' => [
+                    'code' => 200,
+                    'response' => 'failed',
+                    'message' => 'Register new user failed. Please try again.'
+                ],
+                'result' => ''
+            ]);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+  
+        if(auth()->attempt($data)) {
+            $token = auth()->user()->createToken('Laravel8PassportAuth')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+
+    public function userInfo()
+    { 
+        $user = auth()->user();
+      
+        return response()->json(['user' => $user], 200); 
+    }
+}
