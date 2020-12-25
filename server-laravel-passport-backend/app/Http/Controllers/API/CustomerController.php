2a4eec1acbc53ccef4a802c3fae6794cc7f053ca
esuherlan <elansuherlan.es27@gmail.com>
2020-12-25 15:08:43 +0700

    backend: rest api (wip)

--- server-laravel-passport-backend/app/Http/Controllers/API/CustomerController.php
+++ server-laravel-passport-backend/app/Http/Controllers/API/CustomerController.php
@@ -6,8 +6,37 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Customer;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class CustomerController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $customers = [];
+        if(auth()->user()->token) {
+            $customers = Customer::all();
+            return response()->json([
+                'status' => [
+                    'code' => 200,
+                    'response' => 'success',
+                    'message' => 'List of Customers'
+                ],
+                'result' => $customers
+            ]);
+        } else {
+            return response()->json([
+                'status' => [
+                    'code' => 401,
+                    'response' => 'failed',
+                    'message' => 'Unauthenticated.'
+                ],
+                'result' => $customers
+            ]);
+        }
+    }
 }
--- server-laravel-passport-backend/routes/api.php
+++ server-laravel-passport-backend/routes/api.php
@@ -5,25 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PassportAuthController;
 use App\Http\Controllers\API\CustomerController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::post('register', [PassportAuthController::class, 'register']);
-Route::post('login', [PassportAuthController::class, 'login']);
   
 // Route::middleware('auth:api')->group(function () {
-//     Route::get('get-user', [PassportAuthController::class, 'userInfo']); 
 //     Route::resource('customers', [CustomerController::class]); 
 // });
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/customers', [CustomerController::class, 'index']);
+    Route::post('/customers/store', [CustomerController::class, 'store']);
+    Route::get('/customers/{id?}', [CustomerController::class, 'show']);
+    Route::post('/customers/update/{id?}', [CustomerController::class, 'update']);
+    Route::delete('/customers/{id?}', [CustomerController::class, 'destroy']);
+});
